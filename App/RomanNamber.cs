0558a5f4e2b9a8117c59b9a54275afb42bd0d554
kkaat <kkaat@KATERINA>
2024-09-03 12:14:56 +0300

    Add project files.

---
+++ App/RomanNamber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class RomanNamber(int Value)
+    {
+        private readonly int _value = Value;
+        public int Value { get { return _value; } }
+
+        public static RomanNamber Parse(string Value)
+        {
+            return new(0);
+        }
+    }
+}
+
---
+++ Test/RomanNumberTest.cs
@@ -0,0 +1,24 @@
+﻿using App;
+
+namespace Test
+{
+    [TestClass]
+    public class RomanNumberTest
+    {
+        [TestMethod]
+        public void ParseTest()
+        {
+            RomanNamber rn = RomanNamber.Parse("");
+            Assert.IsNotNull(rn, "Parse result is not null");
+            Assert.AreEqual(0, rn.Value, "Zero testing");
+        }
+    }
+}
+/* Тестовий проєкт за структурою відтворює осноний проєкт:
+ * - його папки відповідають папкам основного проєкту
+ * - його класи називають як і проєктн, з дописом Test
+ * - методи класів також відтворюють методи випробуваних класів
+ *      і також дописам Test
+ * Основу тестів складають вислови (Assert)
+ * 
+ */
