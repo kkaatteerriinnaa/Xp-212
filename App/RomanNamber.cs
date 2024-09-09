9a56522a97d269568af4ba0945dfbd5090dc0a43
Катерина Буравлева <kkaatteerriinnaa2003@gmail.com>
2024-09-09 12:01:27 +0300

    Дз2

--- App/RomanNamber.cs
+++ App/RomanNamber.cs
@@ -6,15 +6,50 @@ using System.Threading.Tasks;
 
 namespace App
 {
-    public class RomanNamber(int Value)
+    public class RomanNamber
     {
-        private readonly int _value = Value;
+        private readonly int _value;
         public int Value { get { return _value; } }
 
-        public static RomanNamber Parse(string Value)
+        public RomanNamber(int value)
         {
-            return new(0);
+            _value = value;
+        }
+
+        public static RomanNamber Parse(string value)
+        {
+            int res = 0;
+            int prevDigit = 0;
+            foreach (char c in value.Reverse())
+            {
+                int digit = DigitValue(c);
+                res += digit < prevDigit ? -digit : digit;
+                prevDigit = digit;
+            }
+            return new RomanNamber(res);
+        }
+
+        public static int DigitValue(char digit)
+        {
+            if (!"NIVXLCDM".Contains(digit))
+            {
+                throw new ArgumentException($"The argument '{digit}' is not valid Roman digit.", nameof(digit));
+            }
+
+            return digit switch
+            {
+                'N' => 0,
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => throw new ArgumentException($"The argument '{digit}' is not valid Roman digit.", nameof(digit))
+            };
         }
     }
+
 }
 
--- Test/RomanNumberTest.cs
+++ Test/RomanNumberTest.cs
@@ -8,11 +8,82 @@ namespace Test
         [TestMethod]
         public void ParseTest()
         {
-            RomanNamber rn = RomanNamber.Parse("");
-            Assert.IsNotNull(rn, "Parse result is not null");
-            Assert.AreEqual(0, rn.Value, "Zero testing");
+            Dictionary<string, int> testCases = new()
+        {
+            { "N",      0      },
+            { "I",      1      },
+            { "II",     2      },
+            { "III",    3      },
+            { "IIII",   4      },
+            { "V",      5      },
+            { "X",      10     },
+            { "D",      500    },
+            { "IV",     4      },
+            { "VI",     6      },
+            { "XI",     11     },
+            { "IX",     9      },
+            { "MM",     2000   },
+            { "MCM",    1900   },
+        };
+            foreach (var testCase in testCases)
+            {
+                RomanNamber rn = RomanNamber.Parse(testCase.Key);
+                Assert.IsNotNull(rn, $"Parse result of '{testCase.Key}' is not null");
+                Assert.AreEqual(
+                    testCase.Value,
+                    rn.Value,
+                    $"Parse '{testCase.Key}' => {testCase.Value}"
+                );
+            }
+        }
+
+        [TestMethod]
+        public void DigitValueTest()
+        {
+            Dictionary<char, int> testCases = new()
+        {
+            {'N', 0    },
+            {'I', 1    },
+            {'V', 5    },
+            {'X', 10   },
+            {'L', 50   },
+            {'C', 100  },
+            {'D', 500  },
+            {'M', 1000 },
+        };
+            foreach (var testCase in testCases)
+            {
+                Assert.AreEqual(
+                    testCase.Value,
+                    RomanNamber.DigitValue(testCase.Key),
+                    $"{testCase.Key} => {testCase.Value}"
+                );
+            }
+
+            char[] excCases = { '0', '1', 'x', 'i', '&' };
+
+            foreach (var testCase in excCases)
+            {
+                var ex = Assert.ThrowsException<ArgumentException>(
+                    () => RomanNamber.DigitValue(testCase),
+                    $"DigitValue('{testCase}') must throw Exception"
+                );
+                Assert.IsTrue(
+                    ex.Message.Contains($"{testCase}"),
+                    "DigitValue ex.Message should contain a symbol which caused the exception! " +
+                    $"Symbol: '{testCase}', ex.Message: '{ex.Message}'"
+                );
+                Assert.IsTrue(
+                    ex.Message.Contains("digit") &&
+                    ex.Message.Contains("argument") &&
+                    ex.Message.Contains("not valid Roman digit"),
+                    "DigitValue ex.Message should contain the argument name 'digit', the word 'argument', and the text 'not valid Roman digit'. " +
+                    $"Symbol: '{testCase}', ex.Message: '{ex.Message}'"
+                );
+            }
         }
     }
+
 }
 /* Тестовий проєкт за структурою відтворює осноний проєкт:
  * - його папки відповідають папкам основного проєкту
