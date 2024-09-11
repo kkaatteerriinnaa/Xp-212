8450c7b9352ed122b1443389cc42aa78758df031
Катерина Буравлева <kkaatteerriinnaa2003@gmail.com>
2024-09-12 02:11:30 +0300

    3

--- App/RomanNamber.cs
+++ App/RomanNamber.cs
@@ -20,20 +20,81 @@ namespace App
         {
             int res = 0;
             int prevDigit = 0;
+            int pos = value.Length;
+            int maxDigit = 0;
+            int lessCounter = 0;
+            int maxCounter = 1;
             foreach (char c in value.Reverse())
             {
-                int digit = DigitValue(c);
+                pos--;
+                int digit;
+                try
+                {
+                    digit = DigitValue(c);
+                }
+                catch
+                {
+                    throw new FormatException(
+                        $"{nameof(RomanNamber)}.{nameof(Parse)}() " +
+                        $"found illegal symbol '{c}' in position {pos}");
+                }
+                //"відстань" між цифрами
+                if (digit != 0 && prevDigit / digit > 10)  //цифра занадто "далекі" для віднімання
+                {
+                    throw new FormatException(
+                        $"{nameof(RomanNamber)}.{nameof(Parse)}() " +
+                        $"illegal sequence: '{c}' befor '{value[pos + 1]}' in position {pos}");
+                }
+                // віднімання цифр, що є "5"-ками
+                if(digit < prevDigit && (digit == 5 || digit == 50 || digit == 500))
+                {
+                    throw new FormatException(
+                        $"{nameof(RomanNamber)}.{nameof(Parse)}() " +
+                        $"illegal sequence: '{c}' befor '{value[pos + 1]}' in position {pos}");
+
+                }
+
+                // рахуємо цифри, якщо вони менші за максимальну
+                if (digit > maxDigit)
+                {
+                    maxDigit = digit;
+                    lessCounter = 0;
+                    maxCounter = 1;
+                }
+                else if (digit == maxDigit)
+                {
+                    maxCounter += 1;
+                    lessCounter = 0;
+                }
+                else
+                {
+                    lessCounter += 1;
+                }
+
+                if (lessCounter > 1 || lessCounter > 0 && maxCounter > 1)
+                {
+                    throw new FormatException(value);
+                }
+
+                if (digit == 0 && value.Length > 1)
+                {
+                    throw new FormatException();
+                }
+
                 res += digit < prevDigit ? -digit : digit;
                 prevDigit = digit;
             }
-            return new RomanNamber(res);
+
+            return new(res);
         }
 
         public static int DigitValue(char digit)
         {
             if (!"NIVXLCDM".Contains(digit))
             {
-                throw new ArgumentException($"The argument '{digit}' is not valid Roman digit.", nameof(digit));
+                throw new ArgumentException($"The argument " +
+                    $"'{digit}' is not valid Roman digit.", 
+                    nameof(digit));
             }
 
             return digit switch
@@ -46,7 +107,8 @@ namespace App
                 'C' => 100,
                 'D' => 500,
                 'M' => 1000,
-                _ => throw new ArgumentException($"The argument '{digit}' is not valid Roman digit.", nameof(digit))
+                _ => throw new ArgumentException(
+                    $"RomanNumber.DigitValue() illegal digit: '{digit}'"),
             };
         }
     }
--- Test/RomanNumberTest.cs
+++ Test/RomanNumberTest.cs
@@ -35,6 +35,145 @@ namespace Test
                     $"Parse '{testCase.Key}' => {testCase.Value}"
                 );
             }
+
+            /* виняток парсера - окрім причини винятку містить відомості
+             * про місце виникнення помилки (позиція у рядку)
+             */
+
+            Object[][] exCases = [
+                ["W",   'W', 0],
+                    ["CS",  'S', 1],
+                    ["CX1", '1', 2]
+            ];
+            foreach (var exCase in exCases)
+            {
+                var ex = Assert.ThrowsException<FormatException>(
+                    () => RomanNamber.Parse(exCase[0].ToString()!),
+                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
+                );
+
+
+                //Накладаємо вимоги на повідомлення:
+                //Має містити сам символ, що призводить до винятку.
+                //Має містити позицію символу в рядку.
+                //Має містити назву методу та класу.
+
+                Assert.IsTrue(
+                    ex.Message.Contains($"illegal symbol '{exCase[1]}'"),
+                    $"ex.Message must contain symbol which cause error:" +
+                    $" '{exCase[1]}', ex.Message: {ex.Message}"
+);
+                Assert.IsTrue(
+                    ex.Message.Contains($"in position {exCase[2]}"),
+                    $"ex.Message must contain error symbol position, ex.Message: {ex.Message}"
+                );
+                Assert.IsTrue(
+                    ex.Message.Contains(nameof(RomanNamber)) &&
+                    ex.Message.Contains(nameof(RomanNamber.Parse)),
+                    $"ex.Message must contain names of class and method, ex.Message: {ex.Message}"
+                );
+            }
+
+            Object[][] exCases2 = [
+                    ["VX",   'V', 'X', 0],     //---------
+                    ["LC",   'L', 'C', 0],     // "віднять" між цифрами при відніманні:
+                    ["DM",   'D', 'M', 0],     // відніматись можуть І, Х, С причому від
+                    ["IC",   'I', 'C', 0],     // двох сусідніх цифр (І - від V та X, ...)
+                    ["MIM",  'I', 'M', 1],     //---------
+                    ["MVM",  'V', 'M', 1],
+                    ["MXM",  'X', 'M', 1],
+                    ["CVC",  'V', 'C', 1],
+                    ["MCVC", 'V', 'C', 2],
+                    ["DCIC", 'I', 'C', 2],
+                    ["IM",   'I', 'M', 0]
+            ];
+            foreach (var exCase in exCases2)
+            {
+                var ex = Assert.ThrowsException<FormatException>(
+                    () => RomanNamber.Parse(exCase[0].ToString()!),
+                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
+                );
+
+                Assert.IsTrue(
+                    ex.Message.Contains($"illegal sequence: '{exCase[1]}' befor '{exCase[2]}'"),
+                    $"ex.Message must contain symbol which cause error: '{exCase[1]}' and '{exCase[2]}'" +
+                    $" '{exCase[0]}', ex.Message: {ex.Message}"
+);
+                Assert.IsTrue(
+                    ex.Message.Contains($"in position {exCase[3]}"),
+                    $"ex.Message must contain error symbol position, ex.Message: {ex.Message}"
+                );
+                Assert.IsTrue(
+                    ex.Message.Contains(nameof(RomanNamber)) &&
+                    ex.Message.Contains(nameof(RomanNamber.Parse)),
+                    $"ex.Message must contain names of class and method, ex.Message: {ex.Message}"
+                );
+            }
+
+
+            Object[][] exCases3 = [
+                ["IIX", 'X', 2],    // Перед цифрою є декілька цифр, менших за неї
+                ["VIX", 'X', 2],    // !! кожна пара цифр - правильна комбінація,
+                ["XXC", 'C', 2],    //    проблема створюється щонайменше трьома цифрами
+                ["IXC", 'C', 2],    //
+            ];
+            foreach (var exCase in exCases3)
+            {
+                var ex = Assert.ThrowsException<FormatException>(
+                    () => RomanNamber.Parse(exCase[0].ToString()!),
+                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
+                );
+                Assert.IsTrue(
+                    ex.Message.Contains($"illegal sequence: more than one smaller digits befor '{exCase[1]}'"),
+                    $"ex.Message must contain symbol which cause error: '{exCase[1]}'. " +
+                    $"testCase '{exCase[0]}', testCase: {ex.Message}"
+);
+                Assert.IsTrue(
+                    ex.Message.Contains($"in position {exCase[2]}"),
+                    $"ex.Message must contain error symbol position, testCase: {exCase[0]}"
+                );
+                Assert.IsTrue(
+                    ex.Message.Contains(nameof(RomanNamber)) &&
+                    ex.Message.Contains(nameof(RomanNamber.Parse)),
+                    $"ex.Message must contain names of class and method, testCase: {exCase[0]}"
+                );
+            }
+
+            Object[][] exCases4 = [
+                ["IIX",  'X'],    // Менша цифра після двох однакових
+                ["XCC",  'X'],    // 
+                ["XCC",  'C'],    // 
+                ["XCCC", 'C'],    //
+                ["CXCC", 'C'],    //
+                ["CMM",  'C'],    //
+                ["CMMM", 'C'],    //
+                ["MCMM", 'C'],    //
+
+            ];
+            foreach (var exCase in exCases4)
+            {
+                var ex = Assert.ThrowsException<FormatException>(
+                    () => RomanNamber.Parse(exCase[0].ToString()!),
+                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
+                );
+            }
+            Object[][] exCases5 = [
+                ["NN",    '0'],    // Цифра не може бути у числі, тільки
+                ["IN",    '1'],    // сама по собі
+                ["NX",    '0'],    
+                ["NC",    '0'],    
+                ["XNC",   '1'],    
+                ["IVIN",  '3'],    
+                ["XNNN",  '1'],    
+
+            ];
+            foreach (var exCase in exCases5)
+            {
+                var ex = Assert.ThrowsException<FormatException>(
+                    () => RomanNamber.Parse(exCase[0].ToString()!),
+                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
+                );
+            }
         }
 
         [TestMethod]
